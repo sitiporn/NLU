5d0332e212ff0ee5715de5ee954446448b00ef3f
Rom Parnichkun <rom.parnichkun@gmail.com>
2021-04-18 16:24:03 +0700

    added example at test_rom2

--- test/hey_ac.py
+++ test/hey_ac.py
@@ -38,6 +38,7 @@ class HeyAC:
                 'JJ':[],
                 }
         
+
         if len(parse_trees) > 1:
             print('[WARNING] More than a single parse detected')
 
@@ -103,110 +104,3 @@ class HeyAC:
         '''
         tokens = text.split(' ')
         return tokens
-
-
-class hey_ac():
-    def __init__(self, sent, grammar, var=False):
-        self.orig_sent = sent
-        self.sent = None
-        self.grammar = grammar
-        
-        # Convert words to numbers
-        self.var = var
-        if self.var == True:
-            self.sent = self.sent.split()
-            self.sent = [ "hey_num" if i.isdigit() else i for i in self.sent]
-            self.sent = ' '.join(self.sent)
-        
-        self.parsed = TextBlob(self.sent)
-        self.parsed = self.parsed.lower() #Increase TeMpeRATure -> increase temperature
-
-        self.parsed = self.parsed.correct() # increese -> increase
-        
-        self.sent_prep = []
-
-    def lemmatization(self):
-        # Lemmatization
-        for i in range(len(self.parsed.words)):
-            # lemmatization: colder -> cold 
-            # pos = 'a' --> adj, 'v' --> verb, 'n' -> noun
-            lemma = lemmatizer.lemmatize(self.parsed.words[i], pos ='a')
-            lemma = lemmatizer.lemmatize(lemma, pos ='v')
-            lemma = lemmatizer.lemmatize(lemma, pos ='n')
-            # lemmatization: increases -> increase
-            lemma = lemmatizer.lemmatize(lemma)
-            self.sent_prep.append(lemma)
-        
-        # if contains 'please', remove
-        if 'please' in self.sent_prep:
-            self.sent_prep.remove('please')
-            return self.sent_prep
-        else:
-            return self.sent_prep
-    
-    def wordnet(self, to_parse):
-        sent_wordnet = []
-        self.sent_wordnet_orig = len(to_parse)
-        for word in to_parse:
-            for k,v in verb_net.items():
-                if word in v:
-                    sent_wordnet.append(k)
-                else:
-                    pass
-            for k,v in noun_net.items():
-                if word in v:
-                    sent_wordnet.append(k)
-                else:
-                    pass
-            for k,v in adj_net.items():
-                if word in v:
-                    sent_wordnet.append(k)
-                else:
-                    pass
-            if word in ["the","a","an","off","down","up", "on","to","hey_num","by", "from","a.m","at", "pm", "o'clock"]:
-                sent_wordnet.append(word)
-        if len(sent_wordnet) != self.sent_wordnet_orig:
-            raise Exception("Sorry I do not understand you")
-        else:
-            return sent_wordnet
-    
-    
-    def check(self):
-        self.to_parse = self.lemmatization()
-        self.to_parse = self.wordnet(self.to_parse)      
-        rd_parser = nltk.RecursiveDescentParser(self.grammar)
-        try:
-            for p in rd_parser.parse(self.to_parse):
-                if var == True:
-                    numbers = [i for i in split([self.orig_sent]) if i.isdigit()]
-                    for n in numbers:
-                        p = str(p).replace('hey_num', n, 1)
-                        self.to_parse = str(self.to_parse).replace('hey_num', n, 1)
-                        self.to_parse = self.to_parse.strip('][').split(', ')
-
-                return self.to_parse, p
-        except:
-            raise Exception("Sorry I do not understand you")
-            
-            
-    # Pruning 
-    def classify_me(self):
-        to_prune, p = self.check()
-        VB = []
-        NN = []
-        CO = []
-        RP = []
-        for i in Tree.fromstring(str(p)).subtrees():
-            if i.label() == 'VB':
-                VB.append(i.leaves()[0])
-            elif i.label() == 'NN':
-                NN.append(i.leaves()[0])
-            elif i.label() == 'CO':
-                CO.append(i.leaves()[0])
-            elif i.label() == 'RP':
-                RP.append(i.leaves()[0])
-        print('Use me to classify: ')
-        print('VB', VB)
-        print('NN', NN)
-        print('CO', CO)
-        print('RP', RP)
